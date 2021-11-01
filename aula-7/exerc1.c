29466223a4b1732b2905bf97433dc5ea48bb3e0f
Alan Santos <alanssantos32@gmail.com>
2021-10-31 21:28:14 -0300

    Update aula-7 and add aula-8

--- aula-7/exerc1.c
+++ aula-7/exerc1.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
@@ -8,8 +9,7 @@ int menu();
 int *init_list(int len);
 void fill_list(int *list, int len);
 void show_list(int *list, int len);
-void merge_sort(int *list, int init, int len);
-void merge(int *list, int init, int middle, int len);
+void select_sort(int *list, int len);
 
 int main()
 {
@@ -36,7 +36,7 @@ int main()
         case 1:
             if (list != NULL)
             {
-                merge_sort(list, 0, len);
+                select_sort(list, len);
             }
             else
             {
@@ -121,51 +121,22 @@ void show_list(int *list, int len)
     }
 }
 
-void merge_sort(int *list, int init, int len)
+void select_sort(int *list, int len)
 {
-    int middle;
-    if (init < len)
+    int small, swap, i, j, small_id;
+    for (i = 0; i < len - 1; i++)
     {
-        middle = floor((init + len) / 2);
-        merge_sort(list, init, middle);
-        merge_sort(list, middle + 1, len);
-        merge(list, init, middle, len);
-    }
-}
-
-void merge(int *list, int init, int middle, int len)
-{
-    int *temp, p1, p2, len2, i, j, k;
-    int end1 = 0, end2 = 0;
-    len2 = len - init + 1;
-    p1 = init;
-    p2 = middle + 1;
-    temp = (int *)malloc(len2 * sizeof(int));
-    if (temp != NULL)
-    {
-        for (i = 0; i < len2; i++)
+        small = list[i];
+        for (j = i + 1; j < len; j++)
         {
-            if (!end1 && !end2)
+            if (list[j] < small)
             {
-                if (list[p1] < list[p2])
-                    temp[i] = list[p1++];
-                else
-                    temp[i] = list[p2++];
-                if (p1 > middle)
-                    end1 = 1;
-                if (p2 > len)
-                    end2 = 1;
-            }
-            else
-            {
-                if (!end1)
-                    temp[i] = list[p1++];
-                else
-                    temp[i] = list[p2++];
+                small = list[j];
+                small_id = j;
             }
         }
-        for (j = 0, k = init; j < len2; j++, k++)
-            list[k] = temp[j];
+        swap = list[i];
+        list[i] = list[small_id];
+        list[small_id] = swap;
     }
-    free(temp);
 }
